0d32d36969832a189693be961e0457f9de74fd6a
JadnAzzam <79232169+JadnAzzam@users.noreply.github.com>
2022-01-13 20:10:19 +0200

    Update two_number_sum.cpp

--- two_number_sum.cpp
+++ two_number_sum.cpp
@@ -62,4 +62,19 @@ vector<int> twoNumberSum(vector<int> array, int targetSum) {
 
 
 // Most optimal solution is implemented using hashtables 
-// take O(n) time
+// O(nlog(n)) Time and  O(1) space 
+
+#include <unordered_set> 
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+	unordered_set<int> nums;
+	for(int num : array){
+		int potentialMatch = targetSum - num;
+		if(nums.find(potentialMatch) != nums.end()){
+			return vector<int>{potentialMatch, num};
+		}
+		else{
+			nums.insert(num);
+		}
+	}
+	return{};
+} 
